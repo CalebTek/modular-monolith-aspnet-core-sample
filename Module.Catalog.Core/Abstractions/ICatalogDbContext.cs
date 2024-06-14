de31d457c9bc5c0927ab916c3964c9b632dbbb1c
CalebTek <olawale.odeyemi@decagon.dev>
2024-06-14 16:14:27 +0100

    Update ICatalogDbContext.cs

--- Module.Catalog.Core/Abstractions/ICatalogDbContext.cs
+++ Module.Catalog.Core/Abstractions/ICatalogDbContext.cs
@@ -7,7 +7,7 @@ namespace Module.Catalog.Core.Abstractions
 {
     public interface ICatalogDbContext
     {
-        public DbSet<Brand> Brands { get; set; }
+        DbSet<Brand> Brands { get; set; }
 
         Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
